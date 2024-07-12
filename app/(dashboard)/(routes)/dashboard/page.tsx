40317d60d61f60fb79d535f84cd19011c101dfd4
Efla <eflayilmaz@outlook.com>
2024-07-12 13:15:02 +0300

    coversation page added

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,86 @@
-import Image from "next/image";
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { ArrowRight, Code, Image, MessageSquare, Music, Video } from "lucide-react";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+
+
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation" 
+  },
+  {
+    label: "ImageGeneration",
+    icon: Image,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+    href: "/image" 
+  },
+  {
+    label: "VideoGeneration",
+    icon: Video,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+    href: "/video" 
+  },
+  {
+    label: "MusicGeneration",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/music" 
+  },
+  {
+    label: "CodeGeneration",
+    icon: Code,
+    color: "text-yellow-400",
+    bgColor: "bg-yellow-400/10",
+    href: "/code" 
+  },
+
+];
+
 export default function DashboardPage() {
+  const router = useRouter();
   return (
       <div>
-          <p>DashboardPage(Protected)</p>
+          <div className="mb-8 space-y-4">
+            <h2>
+              Explore the power of AI
+            </h2>
+            <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+                Chat with the smartest AI - Experience the power of AI 
+            </p>
+          </div>
+          <div className="px-4 md: px-20 lg: px-32 space-y-4">
+            {tools.map((tool) => (
+              <Card 
+              onClick={() => router.push(tool.href)}
+              key={tool.href}
+              className="p-4 justify-between hover: shadow-md transition border-black/5 flex items-center cursor-pointer">
+                <div className="flex items-center gap-x-4">
+                  <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                    <tool.icon className={cn("w-8 h-8", tool.color)} />
+                  </div>
+                  <div className="font-semibold">
+                    {tool.label}
+                  </div>
+                </div>
+                <ArrowRight className="w-5 h-5" />
+              </Card>
+
+            ))}
+          </div>
       </div>
   );
 }
+
